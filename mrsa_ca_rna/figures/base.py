06920abc83c1771b19bb2ae112b9744754fed7e5
MrBones1102 <jamespopoli@gmail.com>
2024-06-21 13:54:00 -0700

    Successful generation of variance explained graph using seaborn and "base.py"

--- mrsa_ca_rna/figures/base.py
+++ mrsa_ca_rna/figures/base.py
@@ -2,8 +2,81 @@
 Sets up the base figure upon which subplots will be placed in a manuscript style.
 """
 
+import matplotlib.figure
 import seaborn as sns
 import matplotlib
 from matplotlib import gridspec, pyplot as plt
 import svgutils.transform as st
 
+# C++ anti-grain geometry backend 
+matplotlib.use("AGG")
+
+# axes styles
+matplotlib.rcParams["axes.labelsize"] = 11
+matplotlib.rcParams["axes.linewidth"] = 0.7
+matplotlib.rcParams["axes.titlesize"] = 14
+
+# font styles
+matplotlib.rcParams["font.family"] = ["sans-serif"]
+matplotlib.rcParams["font.sans-serif"] = ["Helvetica"]
+matplotlib.rcParams["font.size"] = 10
+
+# plot area styles
+matplotlib.rcParams["grid.linestyle"] = "dotted"
+
+# legend styles
+matplotlib.rcParams["legend.borderpad"] = 0.25
+matplotlib.rcParams["legend.fontsize"] = 9
+matplotlib.rcParams["legend.framealpha"] = 0.25
+matplotlib.rcParams["legend.handlelength"] = 0.5
+matplotlib.rcParams["legend.handletextpad"] = 0.25
+matplotlib.rcParams["legend.labelspacing"] = 0.2
+matplotlib.rcParams["legend.markerscale"] = 0.7
+
+# svg handling
+matplotlib.rcParams["svg.fonttype"] = "none"
+
+# axes labels
+matplotlib.rcParams["xtick.labelsize"] = 8
+matplotlib.rcParams["xtick.major.pad"] = 1.0
+matplotlib.rcParams["xtick.minor.pad"] = 0.9
+matplotlib.rcParams["ytick.labelsize"] = 8
+matplotlib.rcParams["ytick.major.pad"] = 1.0
+matplotlib.rcParams["ytick.minor.pad"] = 0.9
+
+def setupBase(figsize, gridd, style="whitegrid"):
+    """
+    Sets up base figure for plotting subplots
+    
+    Accepts:
+        figsize (tuple): size of figure in inches
+        gridd (ndarray): subplot dimensions in rows and columns
+        style (str): graph style (default: 'whitegrid')
+
+    Returns:
+        ax (list of matplotlib.figure): list subplots contained in overall figure
+        f (matplotlib.figure): figure object with subplots
+        gs (gridspec.GridSpec): gridded subplots present on figure
+    """
+    sns.set_theme(
+        style=style,
+        font_scale=0.7,
+        color_codes=True,
+        palette="viridis",
+        rc=plt.rcParams
+    )
+
+    f = plt.figure(figsize=figsize, constrained_layout=True)
+    gs = gridspec.GridSpec(**gridd, figure=f)
+
+    x = 0
+    ax = list()
+    while x < gridd["nrows"] * gridd["ncols"]:
+        ax.append(
+            f.add_subplot(
+                gs[x],
+            )
+        )
+        x += 1
+
+    return ax, f, gs
--- mrsa_ca_rna/figures/figure00_pca_explained.py
+++ mrsa_ca_rna/figures/figure00_pca_explained.py
@@ -4,29 +4,60 @@ analysis. We're looking to see diminishing returns after several
 components.
 
 To-do:
-    Make graph look pretty?
-        Implement seaborn.
+    Cleanup after writing more of these figure files
 """
 
 import matplotlib.pyplot as plt
 from mrsa_ca_rna.import_data import form_matrix
 import numpy as np
+import pandas as pd
+from mrsa_ca_rna.figures.base import setupBase
+import seaborn as sns
 
 
 from mrsa_ca_rna.pca import perform_PCA
 
-def figure00():
-
+def figure_00_setup():
+    """ Make and organize the data to be used in genFig"""
     rna_mat = form_matrix()
 
     rna_decomp, components, explained = perform_PCA(rna_mat)
     components = np.arange(1, components+1)
-    fig00 = plt.figure()
-    plt.plot(components, explained)
-    plt.title("'Completeness' of PCA Decomposition" )
-    plt.xlabel("# of Principle Components")
-    plt.ylabel("Fraction of variance explained")
-    fig00.savefig("./output/fig00")
+    data = np.stack([components, explained]).T
+    data = pd.DataFrame(data, columns=["components", "explained"])
+    data["components"] = data["components"].astype("int32")
 
-# debug
-figure00()
+    return data
+
+    # fig00 = plt.figure()
+    # plt.plot(components, explained)
+    # plt.title("'Completeness' of PCA Decomposition" )
+    # plt.xlabel("# of Principle Components")
+    # plt.ylabel("Fraction of variance explained")
+    # fig00.savefig("./output/fig00")
+
+def genFig():
+    """
+    Start making the figure.
+    """
+    fig_size = (3,3)
+    layout = {
+        "ncols": 1,
+        "nrows": 1
+    }
+    ax, f, _ = setupBase(
+        fig_size,
+        layout
+    )
+
+    data = figure_00_setup()
+    a = sns.lineplot(data=data, x="components", y="explained", ax=ax[0])
+    a.set_xlabel("# of Components")
+    a.set_ylabel("Fraction of explained variance")
+    a.set_title("PCA performance")
+
+    return f
+
+# #debug
+# fig = genFig()
+# fig.savefig("./mrsa_ca_rna/output/fig00_withBase.svg")

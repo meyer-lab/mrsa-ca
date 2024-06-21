3f3459567f455d408e3bd2e56203694bb687aba2
MrBones1102 <jamespopoli@gmail.com>
2024-06-21 09:18:15 -0700

    Initial seaborn implementation commit. Creating a "base" for figure generation.

---
+++ mrsa_ca_rna/figures/base.py
@@ -0,0 +1,9 @@
+"""
+Sets up the base figure upon which subplots will be placed in a manuscript style.
+"""
+
+import seaborn as sns
+import matplotlib
+from matplotlib import gridspec, pyplot as plt
+import svgutils.transform as st
+

fixed possible predict_poba data leakage, now uses cross_val_predict() for figure03a and figure08b

mrsa_ca_rna/figures/figure03_RegressionScore.py
@@ -32,9 +32,7 @@ def figure_03_setup(components: int = 60):
     ca_df = whole_data[whole_data.obs["disease"] == "Candidemia"].to_df()
 
     datasets = {"MRSA": mrsa_df, "MRSA+CA+Healthy": combined_df, "CA": ca_df}
-    performance_dict = datasets.copy()
-
-    comp_arr = np.arange(1, components + 1)
+    performance_dict = {}
 
     for dataset in datasets:
         print(f"Performing PCA on {dataset} dataset.")
@@ -71,7 +69,7 @@ def figure_03_setup(components: int = 60):
             )
             performance.append(nested_performance)
 
-        matrix = np.array([comp_arr.astype(int), performance]).T
+        matrix = np.array([range(1, components + 1), performance]).T
         performance_df = pd.DataFrame(
             matrix, columns=["Components", "Nested Performance"]
         )

mrsa_ca_rna/figures/figure03a.py
@@ -10,6 +10,7 @@ This will help tie the regression model back to the original data.
 # imports
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.model_selection import cross_val_predict, StratifiedKFold
 
 from mrsa_ca_rna.pca import perform_PCA
 from mrsa_ca_rna.regression import perform_PC_LR
@@ -19,6 +20,8 @@ from mrsa_ca_rna.import_data import concat_datasets, gene_converter
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+skf = StratifiedKFold(n_splits=10)
 
 
 # setup figure
@@ -39,7 +42,10 @@ def figure03a_setup():
 
     # perform logistic regression on transformed MRSA data
     _, model = perform_PC_LR(mrsa_xform, mrsa_data.obs["status"])
-    y_proba = model.predict_proba(mrsa_xform)
+    # y_proba = model.predict_proba(mrsa_xform)
+    y_proba = cross_val_predict(
+        model, X=mrsa_xform, y=mrsa_data.obs["status"], cv=skf, method="predict_proba"
+    )
 
     # get the beta coefficients from the model
     weights: np.ndarray = model.coef_[0]

mrsa_ca_rna/figures/figure08b.py
@@ -8,11 +8,13 @@ from mrsa_ca_rna.figures.base import setupBase
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.model_selection import cross_val_predict, StratifiedKFold
 
 import pandas as pd
 import numpy as np
 import seaborn as sns
 
+skf = StratifiedKFold(n_splits=10)
 
 def figure08b_setup():
     """Organize data for plotting"""
@@ -41,7 +43,7 @@ def figure08b_setup():
 
     # perform logistic regression on mrsa_loadings data
     _, model = perform_PC_LR(mrsa_loadings, mrsa_y)
-    y_proba = model.predict_proba(mrsa_loadings)
+    y_proba = cross_val_predict(model, X=mrsa_loadings, y=mrsa_y, cv=skf, method="predict_proba")
 
     # get the beta coefficients from the model, arrange them by absolute value, then tie them back to the components
     weights: np.ndarray = model.coef_[0]
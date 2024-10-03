2fc251ebc52309515fe9855dbb86d4004c7c4c95
MrBones1102 <jamespopoli@gmail.com>
2024-10-02 20:56:36 -0700

    pyright, rye fmt, rye lint

--- mrsa_ca_rna/figures/figure12a.py
+++ mrsa_ca_rna/figures/figure12a.py
@@ -1,21 +1,21 @@
 """This file will plot the prediction performance of a logistic regression model
-that predicts what disease category a patient belongs to based on """
+that predicts what disease category a patient belongs to based on"""
+
+import pandas as pd
+import seaborn as sns
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_predict
 
 from mrsa_ca_rna.factorization import perform_parafac2, prepare_data
+from mrsa_ca_rna.figures.base import setupBase
 from mrsa_ca_rna.import_data import (
-    import_healthy,
-    import_breast_cancer,
     concat_datasets,
     concat_general,
+    import_breast_cancer,
+    import_healthy,
 )
-from mrsa_ca_rna.figures.base import setupBase
 from mrsa_ca_rna.regression import perform_PC_LR
 
-import pandas as pd
-import numpy as np
-import seaborn as sns
 
 def figure12a_setup(rank=20):
     """Generate the disease factor matrix using the PARAFAC2 algorithm
@@ -31,15 +31,15 @@ def figure12a_setup(rank=20):
     disease_xr = prepare_data(disease_data, expansion_dim="disease")
 
     tensor_decomp, _ = perform_parafac2(disease_xr, rank=rank)
-    disease_factors = tensor_decomp[1][0] # only the first factor matrix is needed
+    disease_factors = tensor_decomp[1][0]  # only the first factor matrix is needed
 
     # organize disease factors into a pandas dataframe
     disease_df = pd.DataFrame(
         disease_factors,
         index=disease_data.obs["disease"].unique(),
         columns=range(1, rank + 1),
-        )
-    
+    )
+
     # transpose the dataframe and reset index to perform rank-based logistic regression
     disease_df = disease_df.T.reset_index(names="Rank")
 
@@ -48,19 +48,23 @@ def figure12a_setup(rank=20):
         id_vars=["Rank"],
         value_vars=["MRSA", "Candidemia", "Healthy", "BreastCancer"],
         var_name="Disease",
-        value_name="Factor Value")
+        value_name="Factor Value",
+    )
 
     # make mapping between disease and string integers, then remap the Rank column
     mapping = {"MRSA": "1", "Candidemia": "2", "Healthy": "3", "BreastCancer": "4"}
-    melted["Rank"] = melted["Rank"].astype(str) + "_" + melted["Disease"].map(mapping)
+    melted["Rank"] = (
+        melted["Rank"].astype(str) + "_" + melted["Disease"].replace(mapping)
+    )
 
     x_data = melted.loc[:, ["Factor Value"]]
-    y_data = melted["Disease"].values.ravel()
+    y_data = melted.loc[:, "Disease"]
 
     # perform logistic regression on the melted dataframe
-    score, model = perform_PC_LR(x_data, y_data)
+    score, model = perform_PC_LR(x_data, y_data, return_clf=True)
 
     return melted, score, model
+
 
 def genFig():
     """Generate the figure comparing the prediction performance of logistic regression
@@ -74,14 +78,36 @@ def genFig():
         melted, _, model = figure12a_setup(rank=rank)
 
         # plot the confusion matrix for the model overall
-        y_pred = cross_val_predict(model, X=melted.loc[:, ["Factor Value"]], y=melted["Disease"], cv=5)
-        
-        conf_matrix = confusion_matrix(melted["Disease"], y_pred, labels=["MRSA", "Candidemia", "Healthy", "BreastCancer"])
+        y_pred = cross_val_predict(
+            model, X=melted.loc[:, ["Factor Value"]], y=melted["Disease"], cv=5
+        )
 
-        true_labels = ["True MRSA", "True Candidemia", "True Healthy", "True BreastCancer"]
-        pred_labels = ["Predicted MRSA", "Predicted Candidemia", "Predicted Healthy", "Predicted BreastCancer"]
+        conf_matrix = confusion_matrix(
+            melted["Disease"],
+            y_pred,
+            labels=["MRSA", "Candidemia", "Healthy", "BreastCancer"],
+        )
 
-        a = sns.heatmap(conf_matrix, xticklabels=pred_labels, yticklabels=true_labels, annot=True, ax=ax[i])
+        true_labels = [
+            "True MRSA",
+            "True Candidemia",
+            "True Healthy",
+            "True BreastCancer",
+        ]
+        pred_labels = [
+            "Predicted MRSA",
+            "Predicted Candidemia",
+            "Predicted Healthy",
+            "Predicted BreastCancer",
+        ]
+
+        a = sns.heatmap(
+            conf_matrix,
+            xticklabels=pred_labels,
+            yticklabels=true_labels,
+            annot=True,
+            ax=ax[i],
+        )
         a.set_title(f"Using Rank {rank}")
 
     return f

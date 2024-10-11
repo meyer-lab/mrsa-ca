710dc2aa7f5f4843be095f43f85616554deb4447
MrBones1102 <jamespopoli@gmail.com>
2024-10-11 10:29:15 -0700

    pf2 component > 2 failure

--- mrsa_ca_rna/figures/figure10.py
+++ mrsa_ca_rna/figures/figure10.py
@@ -15,7 +15,8 @@ def figure10_setup():
 
     time_xr = prepare_data(time_data, expansion_dim="subject_id")
 
-    tensor_decomp, _ = perform_parafac2(time_xr, rank=2)
+    # going above rank = 2 causes function failure
+    tensor_decomp, _ = perform_parafac2(time_xr, rank=3)
     time_factors = tensor_decomp[1]
 
     return time_factors

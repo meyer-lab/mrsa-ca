cf86df93f62632e4a59282e20dfdff8f7e5b1bf0
MrBones1102 <jamespopoli@gmail.com>
2025-01-28 12:07:54 -0800

    initial fixing

--- mrsa_ca_rna/figures/figure00_pca_explained.py
+++ mrsa_ca_rna/figures/figure00_pca_explained.py
@@ -1,11 +1,6 @@
 """
-Generate a figure depicting the explained variance of the PCA
-analysis. We're looking to see diminishing returns after several
-components.
-
-To-do:
-    Cleanup after writing more of these figure files
-    Regenerate plots using recently added gse_healthy data
+Plots a figure of the explained variance of the PCA components for each dataset.
+Datasets are MRSA, MRSA+CA, and CA.
 """
 
 import numpy as np
@@ -20,27 +15,41 @@ from mrsa_ca_rna.pca import perform_pca
 def figure_00_setup():
     """Make and organize the data to be used in genFig"""
 
-    whole_data = concat_datasets(scale=False, tpm=True)
+    # list the datasets we want to compare and define total components
+    datasets = ["mrsa", "ca"]
+    components = 70
 
-    datasets = {
-        "MRSA": whole_data[whole_data.obs["disease"] == "MRSA"].to_df(),
-        "MRSA+CA": whole_data.to_df(),
-        "CA": whole_data[whole_data.obs["disease"] == "Candidemia"].to_df(),
-    }
+    # grab the combined dataset
+    combined_data = concat_datasets(datasets, scale=False, tpm=True)
 
-    for dataset in datasets:
-        _, _, pca = perform_pca(datasets[dataset])
+    # split it into the datasets we want to compare
+    mrsa_data = combined_data[combined_data.obs["disease"] == "MRSA"].copy()
+    ca_data = combined_data[combined_data.obs["disease"] == "Candidemia"].copy()
 
-        components = np.arange(1, pca.n_components_ + 1, dtype=int)
-        total_explained = np.cumsum(pca.explained_variance_ratio_)
+    # convert the datasets to pd.dataframes to hand to perform_pca
+    combined_data = pd.DataFrame(combined_data.X, index=combined_data.obs.index)
+    mrsa_data = pd.DataFrame(mrsa_data.X, index=mrsa_data.obs.index)
+    ca_data = pd.DataFrame(ca_data.X, index=ca_data.obs.index)
 
-        data = pd.DataFrame(components, columns=pd.Index(["components"]))
-        data["total_explained"] = total_explained
+    # perform PCA on the datasets
+    _, _, combined_pca = perform_pca(combined_data, components)
+    _, _, mrsa_pca = perform_pca(mrsa_data, components)
+    _, _, ca_pca = perform_pca(ca_data, components)
 
-        datasets[dataset] = data
+    # get the cumulative explained variance for each dataset
+    combined_explained = np.cumsum(combined_pca.explained_variance_ratio_)
+    mrsa_explained = np.cumsum(mrsa_pca.explained_variance_ratio_)
+    ca_explained = np.cumsum(ca_pca.explained_variance_ratio_)
 
-    return datasets
+    # create a dataframe to hold the data (1-70, explained variance)
+    variance_explained = pd.DataFrame(
+        np.arange(1, components + 1, dtype=int), columns=pd.Index(["components"])
+    )
+    variance_explained["combined"] = combined_explained
+    variance_explained["mrsa"] = mrsa_explained
+    variance_explained["ca"] = ca_explained
 
+    return variance_explained
 
 def genFig():
     """

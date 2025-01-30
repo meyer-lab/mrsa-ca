402c1cad71d8cfb716c88914994f3b1d75d76266
MrBones1102 <jamespopoli@gmail.com>
2025-01-30 13:54:59 -0800

    brought figure03a.py up to speed with regression.py changes and simplified a bit.

--- mrsa_ca_rna/figures/figure03a.py
+++ mrsa_ca_rna/figures/figure03a.py
@@ -1,15 +1,6 @@
 """
-This file will first perform PCA on CA data,
-    then use the trained PCA model to project MRSA data onto the same space.
-Then, it will perform a logistic regression on the transformed MRSA data
-    to predict MRSA outcome.
-It will then plot the regression performance as a scatter plot
-    of the predicted vs actual MRSA outcome.
-Then, using the logistic regression model,
-    it will barplot the most important components predicting MRSA outcome.
-From there, it will barplot the most important genes that map
-    to the most important components.
-This will help tie the regression model back to the original data.
+This figure shows the ROC results of the combined logistic regression model,
+the top 6 components tanked by predictive ability, and their associated genes.
 """
 
 # imports
@@ -17,57 +8,57 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.metrics import roc_auc_score, roc_curve
-from sklearn.model_selection import StratifiedKFold, cross_val_predict
-from sklearn.preprocessing import StandardScaler
 
 from mrsa_ca_rna.figures.base import setupBase
 from mrsa_ca_rna.import_data import concat_datasets, gene_converter
 from mrsa_ca_rna.pca import perform_pca
 from mrsa_ca_rna.regression import perform_PC_LR
 
-skf = StratifiedKFold(n_splits=10)
+
+def make_roc_curve(y_true: np.ndarray, y_proba: np.ndarray):
+    """Make the ROC curve"""
+
+    # convert y's to integer
+    y_true = y_true.astype(int)
+
+    fpr, tpr, _ = roc_curve(y_true=y_true, y_score=y_proba[:, 1])
+    data = {"FPR": fpr, "TPR": tpr}
+
+    auc = roc_auc_score(y_true, y_proba[:, 1])
+
+    return data, auc
 
 
 # setup figure
 def figure03a_setup():
     """Collect the data required for figure03a"""
-    orig_data = concat_datasets(scale=False, tpm=True)
-    mrsa_data = orig_data[orig_data.obs["disease"] == "MRSA"].copy()
-    ca_data = orig_data[orig_data.obs["disease"] == "Candidemia"].copy()
+    top_n = 6
 
-    # scale MRSA data prior to use
-    X = mrsa_data.X
-    scaled_X = StandardScaler().fit_transform(X)
-    mrsa_data.X = scaled_X
+    # get the data
+    datasets = ["mrsa", "ca"]
+    combined_ad = concat_datasets(datasets, scale=False, tpm=True)
+    y_true = combined_ad.obs.loc[combined_ad.obs["disease"] == "MRSA", "status"]
 
-    # perform PCA on CA data. Scaling is done in PCA function
-    _, ca_loadings, ca_pca = perform_pca(ca_data.to_df())
+    # perform PCA on the combined data
+    combined_df = combined_ad.to_df()
+    patient_comps, gene_comps, _ = perform_pca(combined_df)
 
-    # transform MRSA data using CA's PCA model
-    mrsa_xform = ca_pca.transform(mrsa_data.to_df())
+    # truncate the combined components to MRSA data
+    mrsa_index = combined_ad.obs["disease"] == "MRSA"
+    patient_comps = patient_comps.loc[mrsa_index, :]
 
-    # perform logistic regression on transformed MRSA data
-    _, model = perform_PC_LR(
-        mrsa_xform, mrsa_data.obs.loc[:, "status"], return_clf=True
-    )
-    # y_proba = model.predict_proba(mrsa_xform)
-    y_proba = cross_val_predict(
-        model, X=mrsa_xform, y=mrsa_data.obs["status"], cv=skf, method="predict_proba"
-    )
-
-    # since cross_val_predict can produce a number of types,
-    # we need to make sure we are dealing with an ndarray
-    y_proba = np.array(y_proba)
+    # perform logistic regression on the combined data
+    _, y_proba, model = perform_PC_LR(patient_comps, y_true, splits=20, return_clf=True)
 
     # get the beta coefficients from the model
     weights: np.ndarray = model.coef_[0]
 
-    # get the location of the top 5 most important components
+    # get the location of the top 6 most important components
     # (absolute value to capture both directions)
-    top_weights_locs = np.absolute(weights).argsort()[-5:]
+    top_weights_locs = np.absolute(weights).argsort()[-top_n:]
 
-    # get the top 5 components from the loadings matrix using the locations
-    top_comps: pd.DataFrame = ca_loadings.iloc[top_weights_locs]
+    # make new dataframe containing only the top 5 components
+    top_comps = gene_comps.iloc[top_weights_locs].copy()
 
     # get the weights of the top 5 components
     top_comp_weights = dict(
@@ -77,46 +68,49 @@ def figure03a_setup():
     # get the top 100 genes for each of the top 5 components
     top_comp_genes = {}
     for comp in top_comps.index:
+
+        # find the top 100 genes for current component
         gene_locs = top_comps.loc[comp].abs().nlargest(100).index
+
+        # go back to the top component dataframe and select the genes
         genes_series = top_comps.loc[comp, gene_locs]
+
+        # create a dataframe of the top 100 genes and their weights
         top_comp_genes[comp] = pd.DataFrame(
             {"Gene": genes_series.index, "Weight": genes_series.values}
         )
 
-        # convert EnsemblGeneID to Symbol, then print to csv
+        # convert EnsemblGeneID to Symbol
         top_comp_genes[comp] = gene_converter(
             top_comp_genes[comp], "EnsemblGeneID", "Symbol"
         )
+
+        ## save the top 100 genes to a csv file
         # top_comp_genes[comp].loc[:, "Gene"].to_csv(
         # f"mrsa_ca_rna/figures/figure03a_top_genes_{comp}.csv",
         # index=False,
         # header=False
         # )
 
-    return y_proba, top_comp_weights, top_comp_genes
+    return y_proba, y_true, top_comp_weights, top_comp_genes
 
 
 def genFig():
-    fig_size = (8, 16)
+    fig_size = (8, 12)
     layout = {"ncols": 2, "nrows": 4}
     ax, f, _ = setupBase(fig_size, layout)
 
-    whole_data = concat_datasets(scale=False, tpm=True)
-    mrsa_data = whole_data[whole_data.obs["disease"] == "MRSA"]
-    y_true = mrsa_data.obs["status"].values.astype(int)
+    y_proba, y_true, top_comp_weights, top_comp_genes = figure03a_setup()
 
-    y_proba, top_comp_weights, top_comp_genes = figure03a_setup()
+    data, auc = make_roc_curve(y_true, y_proba)
 
-    # plot the ROC curve
-    fpr, tpr, _ = roc_curve(y_true=y_true, y_score=y_proba[:, 1])
-    data = {"FPR": fpr, "TPR": tpr}
     a = sns.lineplot(data, x="FPR", y="TPR", ax=ax[0])
     a.set_xlabel("False Positive Rate")
     a.set_ylabel("True Positive Rate")
     a.set_title(
         "Classification of MRSA outcomes using 70 component\n"
-        "PCA decomposition of Candidemia data\n"
-        f"AUC: {roc_auc_score(y_true, y_proba[:, 1]):.3f}"
+        "PCA decomposition of Combined data\n"
+        f"AUC: {auc:.3f}"
     )
 
     # plot the top 5 components and their weights
@@ -127,10 +121,11 @@ def genFig():
     a.set_ylabel("Weight")
     a.set_title("Top 5 Components determining prediction accuracy")
 
-    # plot the top 5 genes for each of the top 5 components
+    # plot the top N genes for each of the top_n components
+    N = 10
     for i, comp in enumerate(top_comp_genes):
         a = sns.barplot(
-            data=top_comp_genes[comp].loc[:5, :], x="Gene", y="Weight", ax=ax[i + 2]
+            data=top_comp_genes[comp].loc[:N, :], x="Weight", y="Gene", ax=ax[i + 2]
         )
         a.set_xlabel("Gene")
         a.set_ylabel("Weight")

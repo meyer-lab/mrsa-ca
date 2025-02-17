b2c3f4c9f7c2aa48c3bec9d39973f10475690f51
James Popoli <jrpopoli@g.ucla.edu>
2025-02-16 17:49:04 -0800

    pf2_factorization changes (#40)
    Changes:
      - utils.py
        - moved gene filtering to be after TPM in `concat_datasets`
        - added `check_sparsity()` function
      - factorization.py
    - switched to matcouply `parafac_aoadmm()` from tnesorly's parafac2
    decomposition
        - added `normalize_factors()` to recover tensorly's normalization
      - gitignore
        - prepared for wandb logging from Weights and Biases

--- mrsa_ca_rna/figures/figure11.py
+++ mrsa_ca_rna/figures/figure11.py
@@ -28,20 +28,19 @@ def figure11_setup():
     disease_xr = prepare_data(disease_data, expansion_dim="disease")
     time_xr = prepare_data(time_data, expansion_dim="subject_id")
 
-    # change ranks_d back to range(1, 11) when running the full dataset!
-    ranks_d = range(1, 21)
-    ranks_t = range(1, 21)
+    ranks_d = range(2, 10)
+    ranks_t = range(2, 10)
 
     r2x_d = []
     r2x_t = []
 
     for rank_d in ranks_d:
-        _, rec_errors_d = perform_parafac2(disease_xr, rank=rank_d)
-        r2x_d.append(1 - min(rec_errors_d))
+        _, _, rec_error_d = perform_parafac2(disease_xr, rank=rank_d)
+        r2x_d.append(1 - rec_error_d)
 
     for rank_t in ranks_t:
-        _, rec_errors_t = perform_parafac2(time_xr, rank=rank_t)
-        r2x_t.append(1 - min(rec_errors_t))
+        _, _, rec_error_t = perform_parafac2(time_xr, rank=rank_t)
+        r2x_t.append(1 - rec_error_t)
 
     return r2x_d, r2x_t
 

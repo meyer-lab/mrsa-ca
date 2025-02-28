7d394909e1651cb9bce1da4f399776cfb8441327
MrBones1102 <jamespopoli@gmail.com>
2025-02-27 16:24:39 -0800

    bootstrapping of pf2 parameters

--- mrsa_ca_rna/figures/figure_pf2_fms.py
+++ mrsa_ca_rna/figures/figure_pf2_fms.py
@@ -6,10 +6,11 @@ TODO: Add a loop to test different ranks and L1 strengths.
     Add genFig() to plot FMS vs. rank and L1 strength.
 """
 
-import numpy as np
+import anndata as ad
 import pandas as pd
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
+from sklearn.utils import resample
 from tlviz.factor_tools import factor_match_score
 
 from mrsa_ca_rna.factorization import perform_parafac2
@@ -19,50 +20,54 @@ from mrsa_ca_rna.utils import concat_datasets
 
 def figure_setup():
     """Collect and organize data for plotting. This function will load the data and
-    perform resampling, then pf2 factorization with different ranks and L1 strengths.
-
-    TODO: Make a loop to test different ranks and L1 strengths.
-    """
+    perform resampling, then pf2 factorization to caluclate the factor match score.
+    It will use l1 and rank values determined from wandb exploration."""
 
     disease_list = ["mrsa", "ca", "bc", "covid", "healthy"]
     disease_data = concat_datasets(disease_list, scale=False, tpm=True)
 
-    """Using values from wandb exploration, we will test the stability
-    of the factor matrices
-    The rank and l1 strength are a combination of good fms score >.70
+    """The rank and l1 strength are a combination of good fms score >.70
     and low error <.4"""
     rank = 30
-    l1 = 2e-5
+    l1 = 1.5e-5
 
-    fms_list = []
-    R2X_diff_list = []
-    trials = 30
-    for _i in range(trials):
-        # store the original data and resampled data
-        X = disease_data.copy()
-        X.X = StandardScaler().fit_transform(X.X)
-
-        X_resampled = disease_data.copy()
-        X_resampled = X_resampled[
-            np.random.choice(X_resampled.shape[0], X_resampled.shape[0], replace=True),
-            :,
-        ]
-        X_resampled.X = StandardScaler().fit_transform(X_resampled.X)
-
-        weights_true, factors_true, _, R2X_true = perform_parafac2(
+    # scale and perform the parafac2 factorization on original data
+    X = disease_data.copy()
+    X.X = StandardScaler().fit_transform(X.X)
+    weights_true, factors_true, _, R2X_true = perform_parafac2(
             X,
             condition_name="disease",
             rank=rank,
             l1=l1,
         )
+    # convert the factors to cp_tensors
+    factors_true = (weights_true, factors_true)
+
+    # convert to pd.DataFrame to keep obs ordered and unique
+    df = disease_data.to_df()
+    df.insert(0, "disease", X.obs["disease"].values)
+
+    fms_list = []
+    R2X_diff_list = []
+    trials = 30
+    for _i in range(trials):
+
+        # resample the data
+        df_resampled: pd.DataFrame = resample(df, replace=True)
+
+        # make a unique index
+        df_resampled = df_resampled.reset_index(drop=True)
+
+        # convert back to AnnData and scale
+        df_resampled.index = df_resampled.index.astype(str)
+        X_resampled = ad.AnnData(df_resampled.loc[:, df_resampled.columns != "disease"])
+        X_resampled.obs["disease"] = df_resampled["disease"].to_numpy()
+        X_resampled.X = StandardScaler().fit_transform(X_resampled.X)
 
         # perform the parafac2 factorization on the resampled data
         weights_resampled, factors_resampled, _, R2X_resampled = perform_parafac2(
             X_resampled, condition_name="disease", rank=rank, l1=l1
         )
-
-        # convert the factors to cp_tensors
-        factors_true = (weights_true, factors_true)
         factors_resampled = (weights_resampled, factors_resampled)
 
         # calculate the factor match score
@@ -78,7 +83,7 @@ def figure_setup():
         metrics, index=range(trials), columns=pd.Index(["fms", "R2X_diff"])
     )
 
-    return metrics
+    return metrics, rank, l1
 
 
 def genFig():
@@ -86,13 +91,15 @@ def genFig():
     layout = {"ncols": 1, "nrows": 1}
     ax, f, _ = setupBase(fig_size, layout)
 
-    data = figure_setup()
+    data, rank, l1 = figure_setup()
 
     a = sns.scatterplot(data=data, x="fms", y="R2X_diff", ax=ax[0])
     a.set_xlabel("Factor Match Score")
     a.set_ylabel("R2X Difference (%)")
     a.set_title(
-        "FMS and R2X percent difference of PF2 factor matrices over 30 trials"
+        "FMS and R2X percent difference of PF2 factor matrices"
+        f"over {data.shape[0]} trials.\n"
+        f"Rank: {rank}, L1: {l1}"
     )
 
     return f

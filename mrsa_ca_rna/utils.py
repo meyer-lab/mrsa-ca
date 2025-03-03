97bebd472f3e7d4c9ed30ee8dd2c191fa584fb2e
MrBones1102 <jamespopoli@gmail.com>
2025-03-03 12:26:16 -0800

    updated parafac2 dependency and fixed anndata assignment bug

--- mrsa_ca_rna/utils.py
+++ mrsa_ca_rna/utils.py
@@ -140,7 +140,7 @@ def gene_filter(
         ]
 
     if isinstance(data, ad.AnnData):
-        return data[:, data.var.index.isin(data_filtered.columns)]
+        return data[:, data.var.index.isin(data_filtered.columns)].copy()
     else:
         return data_filtered
 

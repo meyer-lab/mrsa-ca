5bc0406bc6359431d43d7b51c94ecc74451a165f
MrBones1102 <jamespopoli@gmail.com>
2025-03-13 15:37:14 -0700

    fix importing order. rye lint confused by presence of wandb folder in project?

--- mrsa_ca_rna/experiments/wandb_pf2_l1_rank_fms.py
+++ mrsa_ca_rna/experiments/wandb_pf2_l1_rank_fms.py
@@ -8,7 +8,6 @@ import wandb as wb
 from sklearn.preprocessing import StandardScaler
 from tlviz.factor_tools import factor_match_score
 
-import wandb as wb
 from mrsa_ca_rna.factorization import perform_parafac2
 from mrsa_ca_rna.utils import check_sparsity, concat_datasets, resample_adata
 

a8af984339da9923325a8c391d108d5a14d9fe16
MrBones1102 <jamespopoli@gmail.com>
2025-06-05 15:06:45 -0700

    pf2-refactor squashed merge commit of the following:
    commit e1b7d593f0038502703d9599b2b9ddecfe7fd754
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed Jun 4 14:42:31 2025 -0700
    
        standard fig size
    
    commit b7250a62d435945896d9a1ef09f8584fef71b1bd
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed Jun 4 12:39:30 2025 -0700
    
        cannot parallelize pf2
    
    commit bd0b3e46e03a9a5fd0dc9b1745d11522b6aea955
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed Jun 4 12:23:09 2025 -0700
    
        fix r2x
    
    commit ab62b5cb4e50cd8f7839ad61755922f906e6fd03
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed Jun 4 11:58:35 2025 -0700
    
        build errors
    
    commit b5b1c3f99843abdcf6226849ed2bfa1de40b6b53
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed Jun 4 09:56:37 2025 -0700
    
        transform fix
    
    commit 291425070cd2d5dba1c82cc2bd62d65f4eea0f2f
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed Jun 4 09:44:20 2025 -0700
    
        fmt pyright
    
    commit 72f5c13104dbd1cd931ab4984981bdf860c5cf0f
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed Jun 4 09:34:20 2025 -0700
    
        clean
    
    commit 8b84e99e9149fbac739cc4adfa69e20960cc19d4
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Thu May 29 09:08:31 2025 -0700
    
        undo gene ordering
    
    commit 51c117a29b9d016feaa1afa8b9184aada99e33f9
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 28 17:20:12 2025 -0700
    
        remove random state
    
    commit 4e6f901fd3eb60de0e6cbfe997a755717dead1b7
    Merge: 2c7c75e 50cd2c0
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 28 17:19:10 2025 -0700
    
        Merge branch 'main' into issue_pf2_norm
    
    commit 2c7c75ec5f9aa3651b717be37d180f082415c18a
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 28 17:16:48 2025 -0700
    
        reset, careful merge
    
    commit 96b202deb10d586b4ae53ca1d02ac4ba33ccbc9b
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 28 16:51:07 2025 -0700
    
        plot whole gene matrix
    
    commit 620c660f70e46dd8efdddf80e4a50cbab476a924
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 28 14:49:44 2025 -0700
    
        refactor with new simplifications
    
    commit 9e8af954a56074cc1786671f65f7f49a887f87ae
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Tue May 27 10:31:07 2025 -0700
    
        Separated out the gene_filter func to use during non-pf2 analyses
    
    commit 01098c69b9dbc83eaa8055c2ec7e460f02c4b27a
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Fri May 23 17:10:13 2025 -0700
    
        function simplifications
    
    commit b179cabb3fb660fdbab647a7f8d98390442c631a
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Thu May 22 17:22:37 2025 -0700
    
        renamed fms plot
    
    commit f40e4bb2091c9525067c83389d42c1c03cffb1c1
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Thu May 22 17:22:13 2025 -0700
    
        reworked fms dist plotting
    
    commit 0922e873189a8fb60e903be4b524091c65bcc481
    Merge: 9153dab 8570347
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Thu May 22 16:13:22 2025 -0700
    
        Merge branch 'main' into issue_pf2_norm
    
    commit 9153dab3b2bf85e55ef71a5c365bfdbe5f3f2f95
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 21 16:31:58 2025 -0700
    
        removed cross-study healthy, pyright
    
    commit 984d852d513ae0e8c49e49cb5a3a2c25cf2df104
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 21 14:56:29 2025 -0700
    
        Plot all dataset weighted projections
    
    commit 7e09de03f5cd1e1c949d8dd4b30775b5556dfe73
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Wed May 21 13:13:37 2025 -0700
    
        initial commit. projections figure

---
+++ mrsa_ca_rna/figures/figure_pf2_fms.py
@@ -0,0 +1,88 @@
+"""
+This file bootstraps the factor match score and R2X difference of the PF2 factor
+matrices over 30 trials. The rank and l1 values are determined from wandb exploration.
+
+"""
+
+import anndata as ad
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from tlviz.factor_tools import factor_match_score
+
+from mrsa_ca_rna.factorization import perform_parafac2
+from mrsa_ca_rna.figures.base import setupBase
+from mrsa_ca_rna.utils import concat_datasets, resample_adata
+
+
+def factorize(X_in: ad.AnnData, rank: int):
+    X_in.X = StandardScaler().fit_transform(X_in.X)
+
+    weights, factors, _, R2X = perform_parafac2(
+        X_in, condition_name="disease", rank=rank
+    )
+    factors = (weights, factors)
+
+    return factors, R2X
+
+
+def bootstrap_fms(X, rank, target_trials=30):
+    fms_list = []
+    R2X_diff_list = []
+
+    for _ in range(target_trials):
+        # factorize the original and resampled data
+        factors_true, R2X_true = factorize(X, rank)
+        factors_resampled, R2X_resampled = factorize(resample_adata(X), rank)
+
+        # calculate the factor match score
+        factor_match = factor_match_score(
+            factors_true, factors_resampled, consider_weights=False, skip_mode=1
+        )
+
+        # calculate the relative difference in R2X
+        R2X_diff_percent = (R2X_resampled - R2X_true) / R2X_true
+
+        # collect the metrics
+        R2X_diff_list.append(R2X_diff_percent)
+        fms_list.append(factor_match)
+
+    return fms_list, R2X_diff_list
+
+
+def figure_setup():
+    disease_data = concat_datasets()
+
+    rank = 5
+
+    fms_list, r2x_list = bootstrap_fms(disease_data.copy(), rank=rank, target_trials=10)
+
+    metrics = {"fms": fms_list, "R2X_diff": r2x_list}
+    metrics = pd.DataFrame(
+        metrics, index=range(len(fms_list)), columns=pd.Index(list(metrics.keys()))
+    )
+
+    return metrics
+
+
+def genFig():
+    fig_size = (4, 8)
+    layout = {"ncols": 1, "nrows": 2}
+    ax, f, _ = setupBase(fig_size, layout)
+
+    data = figure_setup()
+
+    a = sns.scatterplot(data=data, x="fms", y="R2X_diff", ax=ax[0])
+    a.set_xlabel("Factor Match Score")
+    a.set_ylabel("R2X Difference (%)")
+    a.set_title(
+        "FMS and R2X percent difference of PF2 factor matrices\nRank: 5, Trials: 10"
+    )
+
+    a = sns.kdeplot(data=data, x="fms", clip=(0, 1), ax=ax[1])
+    a.set_xlabel("Factor Match Score")
+    a.set_ylabel("Density")
+    a.set_xlim(0, 1)
+    a.set_title("Distribution of Factor Match Scores")
+
+    return f

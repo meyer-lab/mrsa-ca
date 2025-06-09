dc15517272459babdb0c0789ccca00398ff7518c
MrBones1102 <jamespopoli@gmail.com>
2025-06-09 08:14:19 -0700

    main Squashed commit of the following:
    commit 35b2348e460cd82218f1070bf3b92153b126d8ec
    Author: James Popoli <jrpopoli@g.ucla.edu>
    Date:   Fri Jun 6 20:00:50 2025 -0700
    
        Pf2 refactor (#61)
        - Moved to simpler normalization methods, entirely handled in
        `concat_datasets()`
           - RPM instead of TMM
           - Log2+1 transformation
           - Z-scoring
    
         - Refactored all mrsa+ca subsetting to re-Z the data where necessary
    
         - Simplified commonly used functions and files
           - Focused on removing unused modularity
    
    commit 50cd2c0f4740d4dd0236c6b8a661d16d5c07a4db
    Author: Aaron Meyer <2065146+aarmey@users.noreply.github.com>
    Date:   Wed May 28 09:07:34 2025 -0700
    
        Use TensorLy for Pf2 (#60)
    
        This is faster because it keeps everything on the GPU.
    
    commit 857034796c1ed287d916c4003c42e4c3178c0384
    Author: Aaron Meyer <2065146+aarmey@users.noreply.github.com>
    Date:   Thu May 22 16:10:38 2025 -0700
    
        Simplify import further (#59)
    
        Check that this provides the same results.
    
        ---------
    
        Co-authored-by: MrBones1102 <jamespopoli@gmail.com>

--- mrsa_ca_rna/figures/figure_pf2_factors.py
+++ mrsa_ca_rna/figures/figure_pf2_factors.py
@@ -8,21 +8,15 @@ from mrsa_ca_rna.figures.base import setupBase
 from mrsa_ca_rna.utils import (
     check_sparsity,
     concat_datasets,
-    gene_filter,
 )
 
 
 def figure_setup():
     """Set up the data for the tensor factorization and return the results"""
 
-    rank = 20
+    rank = 5
 
-    datasets = "all"
-
-    disease_data = concat_datasets(
-        datasets,
-        scale=True,
-    )
+    disease_data = concat_datasets()
 
     _, factors, _, r2x = perform_parafac2(
         disease_data,
@@ -60,12 +54,6 @@ def genFig():
     # Check sparsity of the gene factor matrix
     sparsity = check_sparsity(genes_df.to_numpy())
 
-    # grab the top 300 genes
-    top_n = 300
-    genes_df: pd.DataFrame = gene_filter(
-        genes_df.T, threshold=0, method="mean", top_n=top_n
-    ).T
-
     # put the new genes_df back into the disease_factors[2]
     disease_factors[2] = genes_df.values
 
@@ -75,8 +63,6 @@ def genFig():
         disease_ranks_labels,
         False,
     ]
-
-    # plot heatmap of disease factors with independent cmaps
 
     # Set the A matrix colors
     A_cmap = sns.color_palette("light:#df20df", as_cmap=True)
@@ -118,11 +104,7 @@ def genFig():
         xticklabels=disease_ranks_labels,
         yticklabels=disease_labels[2],
     )
-    c.set_title(
-        f"Gene Factor Matrix\n"
-        f"R2X: {r2x:.2f} | Top {top_n} genes\n"
-        f"Sparsity: {sparsity:.2f}"
-    )
+    c.set_title(f"Gene Factor Matrix\nR2X: {r2x:.2f}\nSparsity: {sparsity:.2f}")
     c.set_xlabel(x_ax_label)
     c.set_ylabel(d_ax_labels[2])
 

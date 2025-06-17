fb7e65a80ef252a8e8648d70ec94b07567970639
MrBones1102 <jamespopoli@gmail.com>
2025-06-17 13:23:26 -0700

    gene-plotting squashed commit containing of the following:
    commit c8f14533dd195a13799ea98736794d095ef6ee92
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Tue Jun 17 13:20:39 2025 -0700
    
        cluster comparison
    
    commit 43da25dc2d2a13bbe9588ebb03bfe4cd8bb3c11e
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Tue Jun 17 09:09:31 2025 -0700
    
        plotting functions
    
    commit 0fbf46630a36330b7947990f741f538cf49e4bb0
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Mon Jun 16 15:47:14 2025 -0700
    
        fix pyproject
    
    commit 1dd32f496e06f5b717d51ae475291230ed6e6f25
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Mon Jun 16 15:44:54 2025 -0700
    
        Squashed commit of Embeddings, containing the following:
    
        commit 327e29c626e64d7342896118a6ad40ab7184f908
        Author: MrBones1102 <jamespopoli@gmail.com>
        Date:   Mon Jun 16 13:44:31 2025 -0700
    
            fmt pyright
    
        commit a915da8dd6a4c64e9894b1ce3b1dc0f84722789a
        Author: MrBones1102 <jamespopoli@gmail.com>
        Date:   Thu Jun 12 16:10:07 2025 -0700
    
            save pyright
    
        commit 89949b8d457f8ded453b6459a995306455e5cc46
        Author: MrBones1102 <jamespopoli@gmail.com>
        Date:   Thu Jun 12 14:45:22 2025 -0700
    
            build errors
    
        commit 5fa46a396e2d3065c8b5fff0c9f8b73966c84b8c
        Author: MrBones1102 <jamespopoli@gmail.com>
        Date:   Thu Jun 12 14:25:44 2025 -0700
    
            refactor pf2 figures
    
        commit b2416b7c5e8f372c332b494c28d9a189a3027c65
        Author: MrBones1102 <jamespopoli@gmail.com>
        Date:   Thu Jun 12 12:16:16 2025 -0700
    
            embeddings plot
    
        commit bf241b277fa9e108e77f224ff2863176cdca25ac
        Author: MrBones1102 <jamespopoli@gmail.com>
        Date:   Thu Jun 12 12:13:42 2025 -0700
    
            initial commit. pf2 factorization changes
    
    commit e92f6db356d997456636e26f2e563d943f59bf2a
    Author: MrBones1102 <jamespopoli@gmail.com>
    Date:   Mon Jun 16 15:33:39 2025 -0700
    
        initial commit. ortools import, file creation

---
+++ mrsa_ca_rna/figures/seriate.py
@@ -0,0 +1,157 @@
+"""Seriation - NP-hard ordering of elements in a set given the distance matrix."""
+
+import numpy
+import ortools
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+from packaging.version import Version
+
+__version__ = "1.0.1"
+ortools_version = Version(ortools.__version__)
+ortools8_10 = Version("8.0.0") <= ortools_version < Version("10")
+if not ortools8_10:
+    raise ImportError(
+        "No valid version of ortools installed. Please install ortools 8 or 9."
+    )
+
+
+class IncompleteSolutionError(Exception):
+    """Indicate that a solution for the TSP problem was not found."""
+
+    pass
+
+
+class InvalidDistanceValues(ValueError):
+    """Indicate that the distance array contains invalid values."""
+
+
+def _validate_data(dists: numpy.ndarray):
+    """Check dists contains valid values."""
+    try:
+        isinf = numpy.isinf(dists).any()
+        isnan = numpy.isnan(dists).any()
+    except Exception as e:
+        raise InvalidDistanceValues() from e
+    if isinf:
+        raise InvalidDistanceValues("Data contains inf values.")
+    if isnan:
+        raise InvalidDistanceValues("Data contains NaN values.")
+
+
+def seriate(
+    dists: numpy.ndarray, approximation_multiplier: int = 1000, timeout: float = 2.0,
+) -> list[int]:
+    """
+    Order the elements of a set so that the sum of sequential pairwise distances is minimal.
+
+    We solve the Travelling Salesman Problem (TSP) under the hood.
+    Reference: http://nicolas.kruchten.com/content/2018/02/seriation/
+
+    :param dists: Either a condensed pdist-like or a symmetric square distance matrix.
+    :param approximation_multiplier: Multiply by this number before converting distances \
+                                     to integers.
+    :param timeout: Maximum amount of time allowed to spend for solving the TSP, in seconds. \
+                    This value cannot be less than 0. If timeout is 0 it will try to solve \
+                    the problem with timeout = 1, and double the timeout every time it fails \
+                    until a valid solution is found.
+    :return: list with ordered element indexes, the same length as the number of elements \
+             involved in calculating `dists`.
+    """
+    _validate_data(dists)
+    if timeout > 0:
+        return _seriate(
+            dists=dists,
+            approximation_multiplier=approximation_multiplier,
+            timeout=timeout,
+        )
+    elif timeout < 0:
+        raise ValueError("timeout cannot be negative.")
+    timeout = 1.0
+    route = None
+    while route is None:
+        print("Trying to solve TSP with timeout = %s", timeout)
+        try:
+            route = _seriate(
+                dists=dists,
+                approximation_multiplier=approximation_multiplier,
+                timeout=timeout,
+            )
+        except IncompleteSolutionError:
+            timeout *= 2
+    return route
+
+
+def _seriate(
+    dists: numpy.ndarray, approximation_multiplier=1000, timeout=2.0
+) -> list[int]:
+    """
+    Order the elements of a set so that the sum of sequential pairwise distances is minimal.
+
+    We solve the Travelling Salesman Problem (TSP) under the hood.
+    Reference: http://nicolas.kruchten.com/content/2018/02/seriation/
+
+    :param dists: Either a condensed pdist-like or a symmetric square distance matrix.
+    :param approximation_multiplier: Multiply by this number before converting distances \
+                                     to integers.
+    :param timeout: Maximum amount of time allowed to spend for solving the TSP, in seconds.
+    :return: list with ordered element indexes, the same length as the number of elements \
+             involved in calculating `dists`.
+    """
+    assert dists[dists < 0].size == 0, "distances must be non-negative"
+    assert timeout > 0
+    squareform = len(dists.shape) == 2
+    if squareform:
+        assert dists.shape[0] == dists.shape[1]
+        size = dists.shape[0]
+    else:
+        # dists.shape[0] = (m * (m - 1)) // 2
+        assert len(dists.shape) == 1
+        assert (
+            int(numpy.round(numpy.sqrt(1 + 8 * dists.shape[0]))) ** 2
+            == 1 + 8 * dists.shape[0]
+        )
+        size = int(numpy.round((1 + numpy.sqrt(1 + 8 * dists.shape[0])) / 2))
+
+    manager = pywrapcp.RoutingIndexManager(size + 1, 1, size)
+    routing = pywrapcp.RoutingModel(manager)
+
+    def dist_callback(x, y):
+        x = manager.IndexToNode(x)
+        y = manager.IndexToNode(y)
+
+        if x in (size, y) or y == size:
+            return 0
+        if squareform:
+            dist = dists[x, y]
+        else:
+            # convert to the condensed index
+            if x < y:
+                x, y = y, x
+            dist = dists[size * y - y * (y + 1) // 2 + x - y - 1]
+        # ortools wants integers, so we approximate here
+        return int(dist * approximation_multiplier)
+
+    routing.SetArcCostEvaluatorOfAllVehicles(
+        routing.RegisterTransitCallback(dist_callback)
+    )
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.time_limit.FromMilliseconds(int(timeout * 1000))
+
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
+    )
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+    assignment = routing.SolveWithParameters(search_parameters)
+    if assignment is None:
+        raise IncompleteSolutionError(
+            "No solution was found. Please increase the timeout value or set it to 0."
+        )
+    index = routing.Start(0)
+    route = []
+    while not routing.IsEnd(index):
+        node = manager.IndexToNode(index)
+        if node < size:
+            route.append(node)
+        index = assignment.Value(routing.NextVar(index))
+    return route

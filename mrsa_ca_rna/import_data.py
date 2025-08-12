d61aa751d2c1ac4ec23bf86d9b52ff561e40fbd9
MrBones1102 <jamespopoli@gmail.com>
2025-08-12 14:22:14 -0700

    Import MRSA and CA status labels

--- mrsa_ca_rna/import_data.py
+++ mrsa_ca_rna/import_data.py
@@ -1,21 +1,16 @@
 """
-Import data from various sources, compiled by ARCHS4 and format them
-into AnnData objects.
+Import data from our compiled aligned RNA-seq datasets.
 
-For now, we keep track of the study numbers and the primary diseases represented.
-We use the primary disease to label the dataset for the time being using
-a disease registry. This is primarily to help a human reader associate imports.
+Import MRSA and Candida metadata specifically for persistence predicitons
+downstream.
 
-In the future, we might create a more comprehensive disease registry to
-better keep track of all diseases represented across all datasets.
+For now, we keep track of the study numbers and the primary disease represented
+in all other datasets.
 """
 
-import json
 from os.path import abspath, dirname, join
 
 import anndata as ad
-import h5py as h5
-import numpy as np
 import pandas as pd
 
 BASE_DIR = dirname(dirname(abspath(__file__)))
@@ -23,20 +18,21 @@ BASE_DIR = dirname(dirname(abspath(__file__)))
 
 def parse_metadata(metadata: pd.DataFrame) -> pd.DataFrame:
     """Parses metadata from a DataFrame, extracting key-value pairs from the
-    'characteristics_ch1' column.
-    """
-    metadata_char_ch1 = metadata["characteristics_ch1"].copy()
+    'characteristics' column. Handles both dict and string formats."""
+    characteristics = metadata["characteristics"].copy()
 
     def parse_row(row):
-        if isinstance(row, str):
+        if isinstance(row, dict):
+            return row
+        elif isinstance(row, str):
             items = [item.split(": ", 1) for item in row.split(",") if ": " in item]
             return {k.strip(): v.strip() for k, v in items if len(k) > 0 and len(v) > 0}
         else:
-            return {}
+            raise TypeError(f"Unhandled metadata format: {type(row)} for row {row}")
 
-    parsed_data = metadata_char_ch1.apply(parse_row)
+    parsed_data = characteristics.apply(parse_row)
     result_df = pd.DataFrame.from_records(
-        parsed_data.to_list(), index=metadata_char_ch1.index
+        parsed_data.to_list(), index=characteristics.index
     )
     return result_df
 
@@ -73,19 +69,6 @@ def load_expression_data() -> ad.AnnData:
 
     exp_df = pd.read_csv(file_path, delimiter=",", compression="gzip")
 
-    # Temporarily remove unfinished studies using a list of study IDs
-    studies_to_remove = [
-        "GSE176262",
-        "GSE89403",
-        "GSE116006",
-        "GSE129882",
-        "GSE129882",
-        "GSE198449",
-        "GSE277354",
-        "GSE215865",
-    ]
-    exp_df = exp_df[~exp_df["study_id"].isin(studies_to_remove)]
-
     # Map the disease registry to the data and rename columns
     exp_df["study_id"] = exp_df["study_id"].map(disease_registry)
     exp_df = exp_df.rename(
@@ -101,3 +84,57 @@ def load_expression_data() -> ad.AnnData:
 
     return data_ad
 
+def import_ca_metadata() -> pd.DataFrame:
+    """Imports the metadata for the CA dataset from a JSON file
+    and returns it as a DataFrame for later addition to an anndata."""
+
+    path = join(BASE_DIR, "mrsa_ca_rna", "data", "GSE176262_characteristics.json")
+
+    metadata = pd.read_json(path, orient="index")
+
+    clinical_var = parse_metadata(metadata)
+
+    # Remove all but the Candidemia samples and keep only the phenotype column
+    clinical_var = clinical_var.loc[clinical_var["phenotype"] == "Candidemia", ["phenotype"]]
+
+    # Add a "status" column
+    clinical_var["status"] = "Unknown"
+
+    return clinical_var
+
+def import_mrsa_metadata() -> pd.DataFrame:
+    """Imports the metadata for the MRSA dataset from 3 sources, metadata_mrsa_tfac.txt,
+    metadata_mrsa_tfac_validation.txt, and metadata_mrsa_sra.csv. The tfac files are
+    combined to get a comprehensive metadata DataFrame. Then the SRA metadata is parsed
+    to map patient sample numbers to SRA numbers."""
+
+    # Load TFAC metadata
+    tfac_path = join(BASE_DIR, "mrsa_ca_rna", "data", "metadata_mrsa_tfac.txt")
+    tfac_val_path = join(BASE_DIR, "mrsa_ca_rna", "data", "metadata_mrsa_tfac_validation.txt")
+
+    tfac_metadata = pd.read_csv(tfac_path, sep=",", index_col=0, dtype=str)
+    tfac_val_metadata = pd.read_csv(tfac_val_path, sep=",", index_col=0, dtype=str)
+
+    # Find the common indices between the two DataFrames
+    common_indices = tfac_metadata.index.intersection(tfac_val_metadata.index)
+
+    # Fill in the Unknown values in the TFAC metadata with the validation metadata
+    tfac_metadata.loc[common_indices, "status"] = tfac_val_metadata.loc[common_indices, "status"]
+
+    # Now that "status" is all 1s and 0s, we can convert it to an integer type
+    tfac_metadata["status"] = tfac_metadata["status"].astype(int)
+
+    # Load SRA metadata
+    sra_path = join(BASE_DIR, "mrsa_ca_rna", "data", "metadata_mrsa_sra.csv")
+    sra_metadata = pd.read_csv(sra_path, sep=",", index_col=0)
+
+    # Reorient the SRA metadata to have the Library Name as the index
+    sra_mapping = sra_metadata.reset_index(names=["SRA"]).set_index("isolate")["SRA"].to_dict()
+
+    # Map the SRA numbers to the sample numbers in the TFAC metadata
+    tfac_metadata.index = tfac_metadata.index.map(sra_mapping)
+
+    # Drop any rows in the TFAC metadata that do not have a corresponding SRA number
+    mrsa_labels = tfac_metadata.loc[tfac_metadata.index.notna(), "status"].copy()
+
+    return mrsa_labels
